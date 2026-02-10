6da6449ee5f8b074d49034674eb0e2e9bd109910
JeanBaptisteRAGUES <jbragues31100@gmail.com>
2026-02-10 16:57:46 +0100

    Réintroduction de la table User après sa suppression par erreur.

--- projetagilite/equipements/models.py
+++ projetagilite/equipements/models.py
@@ -20,3 +20,19 @@ class SportProductRelation(models.Model):
 class SportLevelProductRelation(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     level = models.TextField(choices=SportLevel)
+
+class User(AbstractUser):
+    sportsPratique = models.ManyToManyField(
+        Sport,
+        blank=True,
+        related_name="users"
+    )
+
+    niveauSportif = models.CharField(
+        max_length=10,
+        choices=SportLevel.choices,
+        default=SportLevel.BEGINNER
+    )
+
+    def __str__(self):
+        return self.username
