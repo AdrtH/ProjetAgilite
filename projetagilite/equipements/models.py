95472dc5bf1b3a053b1ca74ebf55f59ddca329f9
Astrée <astree.toulotte@gmail.com>
2026-02-10 14:34:52 +0100

    wip models Produit

--- projetagilite/equipements/models.py
+++ projetagilite/equipements/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Sport(models.TextChoices):
+    BADMINTON = "BAD", ("BADMINTON")
+
+class Product(models.Model):    
+    name = models.TextField()
+#    sport_name = models.TextField(choices=Sport)
+
+class SportsOfProduct(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    sport_name = models.TextField(choices=Sport)
