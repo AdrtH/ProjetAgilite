7121b8ee5ad7b1e4b098657441e75bc6e0568743
JeanBaptisteRAGUES <jbragues31100@gmail.com>
2026-02-11 10:26:09 +0100

    Ajout des colonnes 'price' et 'image' dans la table 'Product' ainsi que les tests nécessaires.

--- projetagilite/equipements/models.py
+++ projetagilite/equipements/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Sport(models.TextChoices):
     BADMINTON = "BAD", "Badminton"
@@ -10,6 +11,16 @@ class SportLevel(models.TextChoices):
 
 class Product(models.Model):    
     name = models.TextField()
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0.00,
+        validators=[MinValueValidator(0)]
+    )
+    image = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 class SportProductRelation(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
